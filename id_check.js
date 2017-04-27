f4a5100fd97e960c6ce466f4d85a00d66da31e2d
Victor Bagu <victorbagu@gmail.com>
2017-04-27 02:31:15 +0200

    Add SA ID detail
    Added South African ID exlanations

--- id_check.js
+++ id_check.js
@@ -5,48 +5,49 @@ if (clientCountry === 'ZW'){
 	console.log(clientId);
 
 	// District where ID was collected
-	collectionDistrict = clientId.slice(0, 2); 
+	collectionDistrict = clientId.slice(0, 2);
 	console.log("ID was collected in District " + collectionDistrict);
 
 	// Village of origin
-	familyDistrict = clientid.slice(-2); 
+	familyDistrict = clientId.slice(-2);
 	console.log("Client originates from Village " + familyDistrict);
 
 	// Verify letter
-	queueNumber = clientId.slice(2, -3); 
-	//console.log("ID Queue Number is " + queueNumber);
-	console.log("Alphabet check letter is " + queueNumber % 23);
+	queueNumber = clientId.slice(2, -3);
+	console.log("ID Queue Number is " + queueNumber);
+	//console.log("Alphabet check letter is " + queueNumber % 23);
 
 }
 
 else if (clientCountry === 'ZA'){
-	
+
 	console.log(clientId);
 
 	// Date of Birth
-	yob = clientId.slice(0, 2); 
-	mob = clientId.slice(2, 4); 
-	dob = clientId.slice(4, 6); 
+	var yob = clientId.slice(0, 2);
+	var mob = clientId.slice(2, 4);
+	var dob = clientId.slice(4, 6);
+
+	var gender = clientId.slice(7, 10);
+	var status = clientId.slice(10, 11);
+
+	var clientGender = (gender > 5000) ? "Female" : "Male";
+	var clientStatus = (status) ? "South African" : "Permanent Resident";
+
 	console.log("Client was born on " + dob + "-" + mob + "-" + yob);
+	console.log("Client is " + clientGender);
+	console.log("Client is a " + clientStatus);
 
 }
 
-
-
 }
 
-// Impose Zimbabwean ID numbers
 var clientId1 = '631222666S70';
-var clientId1 = '631126875Y24';
-var clientId1 = '22214781Y00';
-var clientId1 = '24087995A24';
-
 var clientCountry1 = 'ZW';
 
-// Impose South Arican ID numbers
-var clientId2 = '8206160000000';
-
+var clientId2 = '8207165128085';
 var clientCountry2 = 'ZA';
 
 check_id(clientId1, clientCountry1);
+
 check_id(clientId2, clientCountry2);
